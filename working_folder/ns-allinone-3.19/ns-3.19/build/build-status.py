2371ed6648b6286834ff36ad739f1062343bb722
Matthew Schoening <mschoeni@umich.edu>
2026-04-24 18:03:54 -0400

    add files

---
+++ working_folder/ns-allinone-3.19/ns-3.19/build/build-status.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python
+
+# Programs that are runnable.
+ns3_runnable_programs = ['build/scratch/ns3.19-network-load-balance-optimized', 'build/scratch/ns3.19-scratch-simulator-optimized', 'build/scratch/subdir/ns3.19-subdir-optimized']
+
+# Scripts that are runnable.
+ns3_runnable_scripts = []
+
